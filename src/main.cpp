fec5edf7c5f8a424b6dfd65d5ff568d591dbbba4
Josef Adamcik <josef.adamcik@gmail.com>
2019-11-02 19:46:31 +0100

    Remove forgotten i2c address scanning

--- src/main.cpp
+++ src/main.cpp
@@ -118,48 +118,6 @@ void storeState() {
     delay(2);
 }
 
-void scanadr() {
-    byte error, address;
-  int nDevices;
- 
-  Serial.println("Scanning...");
- 
-  nDevices = 0;
-  for(address = 1; address < 127; address++ )
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      nDevices++;
-    }
-    else if (error==4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0) {
-    Serial.println("No I2C devices found\n");
-  } else {
-    Serial.println("done\n");
-  }
- 
-  delay(5000);           // wait 5 seconds for next scan
-}
-
 /**
  * Everything happens in setup, we have nothing in loop because we are sleeping all the time.
  */ 
@@ -217,7 +175,6 @@ void setup() {
     //no ota update (there will be restart after OTA or manual restart if ota was not performed)
     Wire.begin(/*sda*/4, /*scl*/5); //GPIO4, GPIO5
     Wire.setTimeout(500);
-    scanadr();
     measurementReady = measurement.begin();
     if (!measurementReady) {
         Serial.println("Unable to initialize measurement");
